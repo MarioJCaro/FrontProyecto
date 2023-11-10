8cba739356e3810a2eb4f70dc53da58935809f0b
MarioJCaro <mario.caro@estudiantes.utec.edu.uy>
2023-11-10 00:13:31 -0300

    Arreglos en filtros historial ventas

--- src/app/components/historial-ventas/historial-ventas.component.ts
+++ src/app/components/historial-ventas/historial-ventas.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
+import { MatSelectChange } from '@angular/material/select';
 import { MatTableDataSource } from '@angular/material/table';
 import { InfoOrdenModalComponent } from 'src/app/ExtraComponents/info-orden-modal/info-orden-modal.component';
+import { ESTADOS } from 'src/app/constants/estadosOrden.constant';
 import { Orden } from 'src/app/models/orden.model';
 import { OrdenService } from 'src/app/services/orden/orden.service';
 
@@ -23,7 +25,7 @@ export class HistorialVentasComponent {
   pageEvent: PageEvent = {pageIndex: 0, pageSize: 10, length: 0};
   filterField: string = '';
   filterValue: string = '';
-
+  ESTADOS = ESTADOS; 
 
   constructor(
     public dialog: MatDialog,
@@ -31,13 +33,12 @@ export class HistorialVentasComponent {
   ) {}
 
   ngOnInit() {
+    console.log(ESTADOS);
     this.loadOrdenes();
   }
 
-  loadOrdenes(event?: PageEvent) {
-    const pageIndex = event ? event.pageIndex : this.pageEvent.pageIndex;
-    const pageSize = event ? event.pageSize : this.pageEvent.pageSize;
-  
+  loadOrdenes() {
+     
     // Inicializar parámetros de filtro con undefined
     let estado, fecha;
   
@@ -48,7 +49,7 @@ export class HistorialVentasComponent {
       fecha = this.filterValue;
     }
   
-    this.ordenService.getAll(pageIndex + 1, pageSize, undefined, undefined, estado, fecha).subscribe({
+    this.ordenService.getAll(this.pageEvent.pageIndex + 1, this.pageEvent.pageSize, undefined, undefined, estado, fecha).subscribe({
       next: (response) => {
         this.dataSource = new MatTableDataSource(response.items);
         this.totalCount = response.total;
@@ -67,30 +68,38 @@ export class HistorialVentasComponent {
   }
 
   // Actualiza este método para manejar correctamente ambos tipos de eventos
-onFilterValueChange(event: Event | MatDatepickerInputEvent<Date>) {
-  // Verifica si el evento es un evento de fecha
-  if (event instanceof MatDatepickerInputEvent) {
-    // Es un evento de datepicker, maneja la fecha
-    if (this.filterField === 'fecha') {
-      this.filterValue = event.value ? this.formatDate(event.value) : '';
+  onFilterValueChange(event: MatSelectChange | MatDatepickerInputEvent<Date> | Event) {
+    if (event instanceof MatSelectChange) {
+      // El evento proviene del mat-select de estados
+      this.filterValue = event.value;
+    } else if (event instanceof MatDatepickerInputEvent) {
+      // El evento proviene del datepicker de fechas
+      if (this.filterField === 'fecha') {
+        this.filterValue = event.value ? this.formatDate(event.value) : '';
+      }
+    } else {
+      // El evento proviene de un campo de entrada estándar
+      const inputElement = event.target as HTMLInputElement;
+      this.filterValue = inputElement.value;
     }
-  } else {
-    // No es un evento de datepicker, maneja como evento de input estándar
-    const inputElement = event.target as HTMLInputElement;
-    this.filterValue = inputElement.value;
+    this.loadOrdenes(); // Recarga los datos con el nuevo valor de filtro
   }
-  this.loadOrdenes(); // Recarga los datos con el nuevo valor de filtro
-}
+  
+  resetFilters() {
+    this.filterField = '';
+    this.filterValue = '';
+    this.loadOrdenes();
+  }
 
 private formatDate(date: Date): string {
   return new Date(date).toISOString().split('T')[0];
 }
 
+onPaginateChange(event: PageEvent) {
+  this.pageEvent = event;
   
-
-  onPaginateChange(event: PageEvent) {
-    this.loadOrdenes(event);
-  }
+  this.loadOrdenes();
+}
 
   getMesas(orden: Orden): string {
     // Comprueba si 'orden.mesas' está definido y no está vacío
