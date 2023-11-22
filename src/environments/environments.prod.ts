851d498faabb74df7a52fcbcd5f5820b7ebb08f1
kuervou <102867729+kuervou@users.noreply.github.com>
2023-11-22 00:18:23 -0300

    test

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     apiBaseUrl: 'https://puerta-verde-backend-test-qagoh4glsq-rj.a.run.app/api',
     socketUrl: 'https://puerta-verde-backend-test-qagoh4glsq-rj.a.run.app'
+    
   };
   
